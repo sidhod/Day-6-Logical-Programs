3921b75110c5c2223ad7d5c085635b0010f7b14b
unknown <sidhodhank2018@gmail.com>
2022-09-09 15:57:46 +0530

    Give Coupon Number

---
+++ couponNo.java
@@ -0,0 +1,22 @@
+package Logical_Programs;
+
+public class couponNo {
+	public static void main(String[] args) 
+	{
+		System.out.println("--------------Coupon Number-----------------");
+		char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+		int max=100000000;
+		int random=(int) (Math.random()*max);	
+		StringBuffer sb=new StringBuffer();
+		
+		while (random>0)
+		{
+			sb.append(chars[random % chars.length]);
+			random /= chars.length;
+		}
+
+		String couponCode=sb.toString();
+		System.out.println("Coupon Code: "+couponCode);	
+	}
+
+}
